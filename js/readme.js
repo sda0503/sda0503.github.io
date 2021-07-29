83e7490b0a2d8a2c614b05f30a8fde6ca10d3d08
sda0503 <sda0503@naver.com>
2021-07-29 10:07:53 +0900

    readme

---
+++ js/readme.js
@@ -0,0 +1,9 @@
+var reader = new XMLHttpRequest();
+reader.open('GET', './README.md', true);
+reader.overrideMimeType('text/plain; charset=utf-8');
+reader.onload = function() {
+    if (reader.status == 200) {
+        $('#readme pre').text(reader.responseText);
+    };
+};
+reader.send(null);
